cb2cdf320bb8599307f6eeaf3f5962983e131108
Robert Nuorteva <roba@hey.com>
2021-11-15 12:34:23 +0200

    Add proxy for backend connection - phonebook full stack, part 3.11

--- part2/phonebook/src/services/personDB.js
+++ part2/phonebook/src/services/personDB.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://immense-beach-23795.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
