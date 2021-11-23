68248d849b23d1fa486e568d43f4fc7bfab29e0f
Robert Nuorteva <roba@hey.com>
2021-11-23 17:27:45 +0200

    4.5: helper functions and unit tests, step 3

--- part4/bloglist/tests/total_likes.test.js
+++ part4/bloglist/tests/total_likes.test.js
@@ -1,16 +1,17 @@
 const listHelper = require('../utils/list_helper')
+const data = require('./test_data.json')
 
 describe('total likes', () => {
   test('of one value is the value itself', () => {
-    const blogs = [{likes: 10}]
+    const blogs = data.slice(0,1)
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(10)
   })
 
   test('of many values are their sum', () => {
-    const blogs = [{likes: 1}, {likes: 2}, {likes: 3}]
+    const blogs = [...data]
     const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(6)
+    expect(result).toBe(28)
   })
 
   test('of empty value is zero', () => {
