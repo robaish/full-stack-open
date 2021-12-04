e59d6c04ec94bba782142d70425b2dcc219c9534
Robert Nuorteva <roba@hey.com>
2021-12-04 17:19:19 +0200

    4.9*: Blog list tests, step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,17 +4,24 @@ const app = require('../app')
 const api = supertest(app)
 
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('🌈 When getting notes from database:', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
 
-test('there is one blog', async () => {
-  const response = await api.get('/api/blogs')
-  console.log(response.body);
-  expect (response.body).toHaveLength(1)
+  test('there is one blog', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(1)
+  })
+
+  test('unique identifier is named id, not _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogObj = response.body[0]
+    expect(blogObj.id).toBeDefined()
+  })
 })
 
 afterAll(() => {
