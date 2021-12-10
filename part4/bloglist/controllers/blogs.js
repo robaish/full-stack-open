10214549f3001c856a04ba4c31e3e0529f2f4606
Robert Nuorteva <roba@hey.com>
2021-12-10 12:43:50 +0200

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // GET BLOGS
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+  
   res.json(blogs)
 })
 
@@ -16,12 +20,16 @@ blogsRouter.get('/:id', async (req, res) => {
 // ADD BLOG
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
+
+  const user = await User.findById(body.user)
+  console.log('USER INFO:', user)
   
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   if (blog.likes === undefined) {
@@ -29,6 +37,9 @@ blogsRouter.post('/', async (req, res) => {
   }
   
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   res.json(savedBlog)
 })
 
