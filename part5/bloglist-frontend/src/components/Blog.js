693916c31d23ea2d2a251a81f52b60882ecb6ccb
Robert Nuorteva <roba@hey.com>
2021-12-22 11:35:05 +0200

    5.7* Blog list frontend, step7 - step 6 also done earlier

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {`${blog.title} – ${blog.author}`}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const buttonLabel = showDetails ? 'Hide' : 'View'
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div className='blog-container'>
+      <div className='flex flex-jc-sb'>
+        {`${blog.title} – ${blog.author}`}
+        <button onClick={toggleDetails}>{buttonLabel}</button>
+      </div>
+      {showDetails &&
+        <div className='flex flex-vertical'>
+          <span>{blog.url}</span>
+          <span>Likes: {blog.likes} <button>Like</button></span>
+          <span>Added by {blog.user.name}</span>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
