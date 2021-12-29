95e280a233f7aedd4923989e0919e1f0fffb492b
Robert Nuorteva <roba@hey.com>
2021-12-29 16:37:05 +0200

    5.14*: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author but not url and likes by default', () => {
-  const blog = {
-    title: 'Test title',
-    author: 'Test author',
-    url: 'Test url',
-    likes: 1
-  }
+describe('<Blog />', () => {
+  let component
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+  beforeEach(() => {
+    const user = {
+      id:'123456789',
+      username: 'willywonka',
+      name: 'Willy Wonka'
+    }
 
-  const defaults = component.container.querySelector('.testing-blog-defaults')
-  expect(defaults).not.toHaveStyle('display: none')
+    const blog = {
+      title: 'Test title',
+      author: 'Test author',
+      url: 'Test url',
+      likes: 1,
+      user: user
+    }
 
-  const details = component.container.querySelector('.testing-blog-details')
-  expect(details).toBe(null)
+    component = render(
+      <Blog blog={blog} user={user} />
+    )
+  })
+
+  test('renders title and author but not url and likes by default', () => {
+    const defaults = component.container.querySelector('.testing-blog-defaults')
+    expect(defaults).not.toHaveStyle('display: none')
+
+    const details = component.container.querySelector('.testing-blog-details')
+    expect(details).toBe(null)
+  })
+
+  test('url and likes shown when clicking button', () => {
+    const button = component.getByText('View')
+    fireEvent.click(button)
+
+    const details = component.container.querySelector('.testing-blog-details')
+    expect(details).not.toHaveStyle('display: none')
+  })
 
 })
