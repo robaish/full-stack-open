fb3f2ce50d0afd0da9e2ca9e220a27de486bd88f
Robert Nuorteva <roba@hey.com>
2022-02-03 11:38:38 +0200

    8.17 Listing books

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const { loading, error, data } = useQuery(ALL_BOOKS)
+  const [books, setBooks] = useState(null)
+
+  useEffect(() => {
+    if (data) {
+      setBooks(data.allBooks)
+    }
+  }, [data])
 
   if (!props.show) {
     return null
   }
   if (loading) return <p>Loading...</p> 
   if (error) return <p>Error :(</p>
-
-
-  const books = data.allBooks
 
   return (
     <div>
@@ -29,11 +33,11 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map(book =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           )}
         </tbody>
