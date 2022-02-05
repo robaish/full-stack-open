02feab38a467eae1b3c0459906d167da0fd51430
Robert Nuorteva <roba@hey.com>
2022-02-05 13:54:17 +0200

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -5,6 +5,7 @@ import { ALL_BOOKS } from '../queries'
 const Books = (props) => {
   const { loading, error, data } = useQuery(ALL_BOOKS)
   const [books, setBooks] = useState(null)
+  const [genre, setGenre] = useState(null)
 
   useEffect(() => {
     if (data) {
@@ -18,10 +19,23 @@ const Books = (props) => {
   if (loading) return <p>Loading...</p> 
   if (error) return <p>Error :(</p>
 
+  const genres = books.map(b => b.genres).flat()
+  const uniqueGenres = [...new Set(genres)]
+
+  const filteredBooks = genre
+  ? books.filter(b => b.genres.includes(genre))
+  : books
+
   return (
     <div>
       <h2>books</h2>
-
+      <ul style={{padding: '0'}}>
+        <button type="button" onClick={() => setGenre(null)}>all genres</button>
+        {uniqueGenres.map(genre =>
+          <button type="button" key={genre} onClick={() => setGenre(genre)}>{genre}</button>
+        )}
+      </ul>
+      {genre ? <h3>{genre} books</h3> : <h3>all books</h3>}
       <table>
         <tbody>
           <tr>
@@ -33,7 +47,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(book =>
+          {filteredBooks.map(book =>
             <tr key={book.title}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -17,6 +17,7 @@ query {
       name
     }
     published
+    genres
   }
 }
 `
