a8e84b780a4654e9e50b133a5a963007f1d89dab
Robert Nuorteva <roba@hey.com>
2022-02-10 11:26:49 +0200

    9.3 Command line

--- part9/typescript-exercises/bmiCalculator.ts
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -1,14 +1,43 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface bmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArgs = (args: string[]): bmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments - you need to give exactly 2.");
+  if (args.length > 4) throw new Error("Too many arguments - you need to give exactly 2.");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error("Provided values must be numbers.");
+  }
+}
+
+const calculateBmi = (height: number, weight: number) => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
   if (bmi < 18.5) {
-    return 'Underweight';
+    console.log('Underweight');
   } else if (bmi < 25) {
-    return 'Normal (healthy weight)';
+    console.log('Normal (healthy weight)');
   } else if (bmi < 30) {
-    return 'Overweight';
+    console.log('Overweight');
+  } else {
+    console.log('Obese');
   }
-  return 'Obese';
 }
 
-console.log(calculateBmi(180, 74));
+try {
+ const { height, weight } = parseBmiArgs(process.argv);
+ calculateBmi(height, weight); 
+} catch (error) {
+  let errorMessage = 'There\'s been an error.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/typescript-exercises/exerciseCalculator.ts
+++ part9/typescript-exercises/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-interface ExerciseTracker {
+interface ExerciseResults {
   periodLength: number;
   trainingDays: number;
   success: boolean;
@@ -8,16 +8,39 @@ interface ExerciseTracker {
   average: number;
 }
 
-const calculateExercises = (exerciseHours: number[], target: number): ExerciseTracker => {
+interface ExerciseParams {
+  exerciseHours: number[];
+  target: number;
+}
+
+const parseArgs = (args: string[]): ExerciseParams => {
+  if (args.length === 2) throw new Error("Daily target and exercise hours missing.");
+  if (args.length === 3) throw new Error("Exercise hours missing.");
+  
+  const [ , , target, ...exerciseHours ] = args;
+  const allInputsAreNumbers = exerciseHours.every(input => !isNaN(Number(input)));
+  
+  if (!isNaN(Number(target)) && allInputsAreNumbers) {
+    const numberArr = exerciseHours.map(input => Number(input));
+    return {
+      exerciseHours: numberArr,
+      target: Number(target)
+    }
+  } else {
+    throw new Error("All input values must be numbers.");
+  }
+}
+
+const calculateExercises = (exerciseHours: number[], target: number) => {
   const average = exerciseHours.reduce((acc, prev) => acc + prev) / exerciseHours.length;
   const targetDiff = target - average;
   const numberRating = (targetDiff > 0.25) ? 1 : (targetDiff >= 0) ? 2 : 3;
   const ratings: { [key: string]: any } = {
     1: 'Don\'t beat yourself up, rest is important.',
     2: 'Well done.',
-    3: 'Boom, you really crushed it this week!'
+    3: 'Boom, you really crushed it!'
   }
-  
+
   return {
     periodLength: exerciseHours.length,
     trainingDays: exerciseHours.filter(h => h !== 0).length,
@@ -29,4 +52,13 @@ const calculateExercises = (exerciseHours: number[], target: number): ExerciseTr
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { exerciseHours, target } = parseArgs(process.argv);
+  console.log(calculateExercises(exerciseHours, target));
+} catch (error) {
+  let errorMessage = 'There\'s been an error.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
